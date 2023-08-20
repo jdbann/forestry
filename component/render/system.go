a918d701ecd9f2408663166b9944799c57aca896
John Bannister <johnb@hey.com>
2023-08-20 13:59:26 +0100

    Use ecs package to provide render components on entities

---
+++ component/render/system.go
@@ -0,0 +1,23 @@
+package render
+
+import (
+	"time"
+
+	"github.com/jdbann/forestry/pkg/ecs"
+	"github.com/jdbann/forestry/pkg/geo"
+)
+
+type Component struct {
+	Position geo.Point
+	Rune     rune
+}
+
+func (c Component) View() string {
+	return string(c.Rune)
+}
+
+type System struct {
+	ecs.BaseSystem[*Component]
+}
+
+func (s System) Update(_ time.Duration) {}
