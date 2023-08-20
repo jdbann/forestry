3250d2fb192182288f23f0d882017de6ca4075f7
John Bannister <johnb@hey.com>
2023-08-20 13:36:13 +0100

    Setup basic ecs package

---
+++ pkg/ecs/system_test.go
@@ -0,0 +1,65 @@
+package ecs_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/jdbann/forestry/pkg/ecs"
+	"gotest.tools/v3/assert"
+)
+
+func TestEntityComponentSystem(t *testing.T) {
+	system := &countSystem{}
+	entity := &ecs.Entity{}
+
+	runStep(t, "try to get component when not added to entity", func(t *testing.T) {
+		component, ok := ecs.GetComponent[*countComponent](entity)
+		assert.Assert(t, component == nil)
+		assert.Assert(t, !ok)
+	})
+
+	runStep(t, "add entity without component to system", func(t *testing.T) {
+		ok := system.AddComponentsFromEntity(entity)
+		assert.Assert(t, !ok)
+	})
+
+	runStep(t, "get component after adding to entity", func(t *testing.T) {
+		ecs.AddComponent[*countComponent](entity, &countComponent{})
+		component, ok := ecs.GetComponent[*countComponent](entity)
+		assert.Assert(t, component != nil)
+		assert.Assert(t, ok)
+	})
+
+	runStep(t, "add component from entity to system", func(t *testing.T) {
+		ok := system.AddComponentsFromEntity(entity)
+		assert.Assert(t, ok)
+	})
+
+	runStep(t, "update system and confirm component was updated", func(t *testing.T) {
+		system.Update(0)
+		component, ok := ecs.GetComponent[*countComponent](entity)
+		assert.Assert(t, ok)
+		assert.Equal(t, component.count, 1)
+	})
+}
+
+type countComponent struct {
+	ecs.BaseComponent
+	count int
+}
+
+type countSystem struct {
+	ecs.BaseSystem[*countComponent]
+}
+
+func (s *countSystem) Update(_ time.Duration) {
+	for _, component := range s.BaseSystem.Components {
+		component.count++
+	}
+}
+
+func runStep(t *testing.T, name string, fn func(t *testing.T)) {
+	if !t.Run(name, fn) {
+		t.FailNow()
+	}
+}
