6c48b147fbf9efe5aa0c1325eb9ad3a5eea8dc18
John Bannister <johnb@hey.com>
2024-03-13 11:03:40 +0000

    Add tree entity

---
+++ entity/tree/tree.go
@@ -0,0 +1,16 @@
+package tree
+
+import (
+	"github.com/jdbann/forestry/component/render"
+	"github.com/jdbann/forestry/pkg/ecs"
+	"github.com/jdbann/forestry/pkg/geo"
+)
+
+func New(at geo.Point) *ecs.Entity {
+	return ecs.NewEntity(
+		&render.Component{
+			Position: at,
+			Rune:     '∆',
+		},
+	)
+}
